20391c366cf2e86057f6d949be30e7559394dd8a
nafis195 <nafisbuet1@gmail.com>
2022-07-31 00:02:06 -0700

    web file save

---
+++ test23_WebFileSave.py
@@ -0,0 +1,18 @@
+# Bismillahir Rahmanir Rahim
+
+
+import requests
+import os
+import webbrowser as wb
+
+
+url = "http://dimikcomputing.com"
+responses = requests.get(url)
+
+with open ("dimik.html", "w", encoding=responses.encoding) as f:
+    f.write(responses.text)
+
+file_path = os.path.realpath("dimik.html")
+print(file_path)
+
+wb.open("file://" + file_path)
